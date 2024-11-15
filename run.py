e3210bdcbce54b9e8ce09b9746b2b845e2267dbe
Hardik Srivastava <hardik.srivastava2007@gmail.com>
2024-11-15 16:24:21 +0530

    Update server configuration for Railway deployment

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 from application import app
+import os
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Development server
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
